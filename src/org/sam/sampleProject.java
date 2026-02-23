50a658a4dada3326357acf703b32036b0db70a22
smabdulbasith26 <smabdulbasith26@gmail.com>
2026-02-23 20:34:08 +0530

    done

--- src/org/sam/sampleProject.java
+++ src/org/sam/sampleProject.java
@@ -1,5 +1,18 @@
 package org.sam;
 
 public class sampleProject {
+	
+	public static void gitData() {
+		System.out.println("Git Data");
+
+	}
+	public static void main(String[] args) {
+		
+		gitData();
+		
+		int a = 12345;
+		System.out.println(a);
+		
+	}
 
 }
